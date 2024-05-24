07b8a829b58d717e07ebbc9e0bb3b2ad2e36a6d0
ankurjbs <ankurjbs@gmail.com>
2024-05-24 11:57:39 +0530

    Liftup the fields component liftup the handleChange funtion from field com. to  app com. and theirs userInput state also

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Fields from "./components/Fields";
 import Result from "./components/Result";
+
 function App() {
-  function handleChangeInput(event){
-    console.log(event.target.value);
+
+  const [userInput,setUserInput] = useState({
+    initialInvestment : 10000,
+    annualInvestment : 1200,
+    expectedReturn : 6,
+    duration : 10
+  });
+
+  function handleChange(inputIdentifier,newValue){
+    setUserInput(prevInput => {
+      return {
+         ...prevInput,
+          [inputIdentifier]:newValue
+        }        
+      });
+      console.log(userInput);
   }
+
   return (
     <main>
       <Header />
-      <Fields />
+      <Fields userInput={userInput} onChange={handleChange}/>
       <Result />
     </main>
   );
--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -1,42 +1,25 @@
-import { useState } from "react";
-
-export default function Fields() {
-  const [userInput,setUserInput] = useState({
-    initialInvestment : 10000,
-    annualInvestment : 1200,
-    expectedReturn : 6,
-    duration : 10
-  });
-
-  function handleChange(inputIdentifier,newValue){
-    setUserInput(prevInput => {
-      return {
-         ...prevInput,
-          [inputIdentifier]:newValue
-        }        
-      });
-      console.log(userInput);
-  }
+export default function Fields({onChange,userInput}) {
+  
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="">Initial Investment</label>
-          <input type="number" required onChange={(event)=>handleChange('initialInvestment',event.target.value)} value={userInput.initialInvestment}/>
+          <input type="number" required onChange={(event)=>onChange('initialInvestment',event.target.value)} value={userInput.initialInvestment}/>
         </p>
         <p>
           <label htmlFor="">Annual Investment</label>
-          <input type="number" required onChange={(event)=>handleChange('annualInvestment',event.target.value)} value={userInput.annualInvestment}/>
+          <input type="number" required onChange={(event)=>onChange('annualInvestment',event.target.value)} value={userInput.annualInvestment}/>
         </p>      
       </div>
       <div className="input-group">
         <p>
           <label htmlFor="">Expected Return</label>
-          <input type="number" required onChange={(event)=>handleChange('expectedReturn',event.target.value)} value={userInput.expectedReturn}/>
+          <input type="number" required onChange={(event)=>onChange('expectedReturn',event.target.value)} value={userInput.expectedReturn}/>
         </p>
         <p>
           <label htmlFor="">Duration</label>
-          <input type="number" required onChange={(event)=>handleChange('duration',event.target.value)} value={userInput.duration}/>
+          <input type="number" required onChange={(event)=>onChange('duration',event.target.value)} value={userInput.duration}/>
         </p>      
       </div>
     </section>
